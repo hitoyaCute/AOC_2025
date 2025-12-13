e8bd4bcfb786e77709fd53099d6d10dfa68acd5b
hitoya <nokimberly74@gmail.com>
2025-12-13 20:33:36 +0800

    day 7 and template

--- day_07/main.cpp
+++ day_07/main.cpp
@@ -4,137 +4,71 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
-#include <memory>
 #include <string>
 #include <sys/types.h>
+#include <unordered_set>
 #include <vector>
 #include <filesystem>
 
-template <typename printable>
-void printarray(std::vector<printable> arr) {
-    for (auto i: arr) {
-        std::cout << i << std::endl;
-    }
-}
-
-std::vector<std::string> split(const std::string& s, const char delim = 0) {
-    std::vector<std::string> out{};
-    std::string temp;
-
-    if (s.empty()){
-        return out;
-    }
-    for (const char c : s) {
-        if (delim == 0) {
-            out.push_back(std::string(c,1));
-        } else if (c == delim and temp.length() != 0) {
-            out.push_back(temp);
-            temp.clear();
-        } else if (c != delim) {
-            temp += c;
-        }
-    }
-    temp.empty() ? (void)0: out.push_back(temp);
-    
-    return out;
-}
+template<typename printable>void printarray(std::vector<printable>&arr){for(auto i:arr){std::cout<<i<<std::endl;}}
+std::vector<std::string>split(const std::string&s,const char d=0){std::vector<std::string>o;std::string t;if(s.empty()){return o;}for(const char c:s){if(d==0){o.push_back(std::string(c,1));}else if(c==d and t.length()!=0){o.push_back(t);t.clear();}else if(c!=d){t+=c;}}t.empty()?(void)0:o.push_back(t);return o;}
+std::vector<std::string>parse_file(std::ifstream&f){std::vector<std::string>o;std::string l;while(std::getline(f,l)){if(l.empty())continue;o.push_back(l);}return o;}
 
 
-std::vector<std::string> parse_file(std::ifstream& file) {
-    std::vector<std::string> output;
-    std::string line;
-    while (std::getline(file, line)) {
-        if (line.empty()) continue;
-        output.push_back(line);
-    }
-    return output;
-}
-
-
-struct traveler {
-    std::array<uint8_t,2> pos = {0,0};
-    traveler() = default;
-    traveler(std::array<uint8_t,2> inp): pos{inp}{}
-    traveler(uint8_t x, uint8_t y): pos{x,y}{}
-
-    /*
-    move 1 strp down but return true if its deletable means it hit a splitter or out of bound
-    this will update the data on the dumps if its deletable
-    */
-    bool move(std::vector<std::string>& board, std::unique_ptr<traveler>& dumps) {
-        int width, heigth;
-        width = board[0].length();
-        heigth = board.size();
-        if (0 <= pos[0] and pos[0] <= width and // check if x is in bound
-            0 <= pos[1] and pos[1] <= heigth) { // check if y is in bound
-            switch (board[pos[1]+1][pos[0]]) {
-                case '^': // splitter
-                    pos[0] -= 1;
-                    // *dumps = traveler{pos[0],static_cast<uint8_t>(pos[1]+1)};
-                    return true;
-                case '.':// move down and do nothing
-                    pos[1] += 1;
-                    // dumps.reset();
-                    return false;
-                // just unknown but continue anyways
-                default: dumps.reset(); std::cout << board[pos[1]+1][pos[0]] << std::endl;return false;
-            }
-        // outside of the board
-        } else dumps.reset();return true;
-    }
-};
-
+const inline uint64_t dxy_to64(const uint32_t x,const uint32_t y) {const auto nx=((uint64_t)x)<<32;return nx | (uint64_t)y;}
+const inline uint64_t dxy_to64(const std::array<uint32_t,2> v){return dxy_to64(v[0],v[1]);}
+const inline std::array<uint32_t,2>d64_toxy(const uint64_t v){const uint32_t x = v>>32;return {x, (uint32_t)v & 0xffffffff};}
 
 void part1(std::vector<std::string>& data) {
     
-    int width = {0};
+    long total= 0;
+    uint width = {0};
+    std::unordered_set<uint64_t> travelers{};
+
     if (not data.empty())
         if (not data.begin() -> empty())
             width = data.begin()->length();
-    std::vector<traveler> travelers = {};
   
     // first search where the stater is located
     for (uint8_t i{0}; i<width; i++) {
         if (data[0][i] == 'S'){
             printf("starter is found at %d\n", i);
             fflush(stdout);
-            travelers.push_back({i,0});
+            travelers.insert({i,0});
             break;
         }
     }
-
-    auto travelers_c = travelers;
-
-    int total{0};
-    int y = 0;
-    while (true) {
-        int s = 0;
-        for (int i{0}; i < travelers.size(); i++) {
-
-            auto& t = travelers[i];
-            std::unique_ptr<traveler> second = std::make_unique<traveler>(t);
-            // if traveler can be remove and if it created a new traveler
-            bool r = t.move(data, second);
-            printf("%d traveler's spos %d, %d size %d\n", i, travelers[i].pos[0], travelers[i].pos[1], r);
-            fflush(stdout);
-            // hit a splitter
-            if (r and second) {
-                t.pos[0] -= 1; // change the current element
-                // travelers_c.push_back(t); // and push to the vector
-                travelers_c.push_back(*second); // add new element
-                s += 1;
-            // out side the bounds
-            } else if (r and second == 0) {// removable but did not hit  splitter
-                std::cout << "deleting" << std::endl;
-                travelers_c.erase(travelers.begin() + i);
-            // if both are 0
+    // perform a step
+    uint line = 0;
+    for (; line < data.size(); line++) {
+        int stotal = 0;
+        std::unordered_set<uint64_t> next_travelers{};
+        for (const auto& d: travelers) {
+            std::array<uint32_t,2> pos = d64_toxy(d);
+            char land = data[pos[1]][pos[0]];
+            if (land == '^') {
+                pos[1] += 1;
+                // check which direction the new traveler will move
+                int8_t npos = pos[0] < width - 1? -1 : 1;
+                auto ntrav = pos;
+                ntrav[0] += npos;
+                next_travelers.insert(dxy_to64(ntrav)); // then add the new traveler
+                // check if the  pos is not out of bound then include it and increment stotal, else do nothing
+                pos[0]--;
+                if (0 <= pos[0]) {
+                    stotal++; // inc the sub total
+                    next_travelers.insert(dxy_to64(pos));
+                }
+            } else if (land == '.') {
+                pos[1] += 1;
+                next_travelers.insert(dxy_to64(pos));
             }
         }
-        travelers = travelers_c;
-        if (++y >= width) break;
-        total += s;
+        travelers = next_travelers;
+        total += stotal;
     }
-    std::printf("total %d", total);
+
+    std::printf("total %ld", total);
 }
 
 
--- template.cpp
+++ template.cpp
@@ -7,47 +7,11 @@
 #include <vector>
 #include <filesystem>
 
-template <typename printable>
-void printarray(std::vector<printable> arr) {
-    for (auto i: arr) {
-        std::cout << i << std::endl;
-    }
-}
-
-std::vector<std::string> split(const std::string& s, const char delim = 0) {
-    std::vector<std::string> out{};
-    std::string temp;
-
-    if (s.empty()){
-        return out;
-    }
-    for (const char c : s) {
-        if (delim == 0) {
-            out.push_back(std::string(c,1));
-        } else if (c == delim and temp.length() != 0) {
-            out.push_back(temp);
-            temp.clear();
-        } else if (c != delim) {
-            temp += c;
-        }
-    }
-    temp.empty() ? (void)0: out.push_back(temp);
-    
-    return out;
-}
+template<typename printable>void printarray(std::vector<printable>&a){for(auto i:a){std::cout<<i<<std::endl;}}
+std::vector<std::string>split(const std::string&s,const char d=0){std::vector<std::string>o;std::string t;if(s.empty()){return o;}for(const char c:s){if(d==0){o.push_back(std::string(c,1));}else if(c==d and t.length()!=0){o.push_back(t);t.clear();}else if(c!=d){t+=c;}}t.empty()?(void)0:o.push_back(t);return o;}
+std::vector<std::string>parse_file(std::ifstream&f){std::vector<std::string>o;std::string l;while(std::getline(f,l)){if(l.empty())continue;o.push_back(l);}return o;}
 
 
-
-
-std::vector<std::string> parse_file(std::ifstream& file) {
-    std::vector<std::string> output;
-    std::string line;
-    while (std::getline(file, line)) {
-        if (line.empty()) continue;
-        output.push_back(line);
-    }
-    return output;
-}
 
 
 void part1(std::vector<std::string>& data) {
